67db052134b88b3df0baf0673ac8c5ca901cfff2
Paudom <pavdom72@gmail.com>
2019-10-06 13:50:11 +0200

    Final version

--- week1/main.py
+++ week1/main.py
@@ -7,8 +7,8 @@ from evaluation import EvaluationT1,EvaluationT5
 import os
 
 # -- CONSTANTS -- #
-qs1_root = "../qs1t"
-qs2_root = "../qs2"
+qs1_root = "../qs1"
+qs2_root = "../qs2t"
 masks_path = "../results/QS2_masks"
 db_root = "../database"
 results_root = "../results"
@@ -19,10 +19,6 @@ if __name__ == '__main__':
 
     option = int(input('Specify the dataset:\n\t Both(0):\n\t QS1(1):\n\t QS2(2):\n\t Input:'))
     if option not in available:
-        # -- EVALUATE F1-SCORE FOR THE QS2 MASKS -- #
-        eval_t5 = EvaluationT5(res_path=masks_path,
-                            gt_path=qs2_root)
-        eval_t5.compute_fscore()
         raise Exception('Option not supported')
 
     db_descriptor = GenerateDescriptorsGrid(db_root)
@@ -41,7 +37,7 @@ if __name__ == '__main__':
         qs1_searcher = Searcher(data_path=results_root+os.sep+"db_desc1.pkl",
                             query_path=results_root+os.sep+"qs1_desc.pkl")
         qs1_searcher.search(limit=10)
-        qs1_searcher.save_results(results_root,"result.pkl")
+        qs1_searcher.save_results(results_root,"qs1_results.pkl")
 
         # -- EVALUATE RESULTS FOR TASK1 FOR QS1 -- #
         eval_t1 = EvaluationT1(query_res_path=results_root+os.sep+"qs1_results.pkl",
