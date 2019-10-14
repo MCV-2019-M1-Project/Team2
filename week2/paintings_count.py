b030295fe59d45036627dbb509cc9a195e92d3d9
RogerCasalsVilardell <43968704+RogerCasalsVilardell@users.noreply.github.com>
2019-10-14 21:35:37 +0200

    + strategy for counting paintings based on edges

--- week2/paintings_count.py
+++ week2/paintings_count.py
@@ -5,11 +5,16 @@ import cv2
 import numpy as np
 from background_removal import BackgroundMask2
 
+# Parameters MASK method
 CLOSING_KERNEL_SIZE = 10
 OPENING_KERNEL_SIZE = 40
 THRESHOLD_TO_CONSIDER_SWITCH_TO_WHITE_REGION = 0.6
 THRESHOLD_TO_CONSIDER_SWITCH_TO_BLACK_REGION = 0.5
 RESIZE_SHAPE = (400, 400)
+
+# Parameters EDGES method
+MINIMUM_BLACK_WIDTH = 0.1
+MINIMUM_WHITE_WIDTH = 0.1
 
 
 def computeMaskToCountPaintings(img_path):
@@ -35,6 +40,24 @@ def computeMaskToCountPaintings(img_path):
 
     return filtered_mask
 
+def computeEdgesToCountPaintings(img_path):
+    # Read image and resize
+    img = cv2.imread(img_path)
+    original_size = img.shape[:2]
+    img = cv2.resize(img,(1000,1000))
+
+    # Obtain gray level image
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+    # Obtain edges through Canny algorithm
+    edges = cv2.Canny(gray,50,150,apertureSize=3)
+
+    # Dilate edges and resize
+    kernel = np.ones((3,3),np.uint8)
+    edges = cv2.dilate(edges,kernel,iterations=1)
+    edges = cv2.resize(edges,(original_size[1],original_size[0]))
+
+    return edges
 
 def countNumberPaintingsBasedOnMask(mask, img_path):
     print("\nCounting number of paintings in img_path: ", img_path)
@@ -65,51 +88,110 @@ def countNumberPaintingsBasedOnMask(mask, img_path):
 
     print("Count of paintings: " + str(number_of_white_predominant_regions))
     pixel_count_cut = pixel_count_end_of_first_painting + ((pixel_count_start_of_second_painting - pixel_count_end_of_first_painting) / 2)
+    
+    orig_width = cv2.imread(img_path).shape[1]
+    pixel_count_cut = int((pixel_count_cut / RESIZE_SHAPE[1]) * orig_width)
+
     return number_of_white_predominant_regions, pixel_count_cut
 
+def countNumberPaintingsBasedOnEdges(mask, img_path):
+    print("\nCounting number of paintings in img_path: ", img_path)
+    height = mask.shape[0]
+    width = mask.shape[1]
+    white_threshold = 0
+    black_threshold = 1
+
+    black_region_length = 0
+    white_region_length = 0
+    number_of_white_predominant_regions = 0
+    pixel_count_end_of_first_painting = -1
+    pixel_count_start_of_second_painting = -1
+    current_region = colorRegion.BLACK
+    print("Looping on width: ", width)
+    for i in range(0, width):
+        vertical_pixel_line = mask[:, i]
+        assert len(vertical_pixel_line) == height
+        white_pixel_count = np.count_nonzero(vertical_pixel_line)
+
+        if current_region == colorRegion.BLACK:
+            black_region_length += 1
+            white_region_length = 0
+            if white_pixel_count > white_threshold and pixel_count_end_of_first_painting > 0:
+                if black_region_length > MINIMUM_BLACK_WIDTH*width:
+                    current_region = colorRegion.WHITE
+                    number_of_white_predominant_regions += 1
+                    if pixel_count_end_of_first_painting > 0:
+                        pixel_count_start_of_second_painting = i
+                else:
+                    continue
+            elif white_pixel_count > white_threshold:
+                current_region = colorRegion.WHITE
+                number_of_white_predominant_regions += 1
+                if pixel_count_end_of_first_painting > 0:
+                    pixel_count_start_of_second_painting = i
+        elif current_region == colorRegion.WHITE:
+            black_region_length = 0
+            white_region_length += 1
+            if white_pixel_count < black_threshold:
+                if white_region_length > MINIMUM_WHITE_WIDTH*width:
+                    if pixel_count_start_of_second_painting <= 0:
+                        pixel_count_end_of_first_painting = i
+                else:
+                    number_of_white_predominant_regions -= 1
+                current_region = colorRegion.BLACK
+
+    if current_region == colorRegion.WHITE and white_region_length < MINIMUM_WHITE_WIDTH*width:
+        number_of_white_predominant_regions -= 1
+
+    if number_of_white_predominant_regions > 1:
+        print(pixel_count_end_of_first_painting,pixel_count_start_of_second_painting)
+
+    print("Count of paintings: " + str(number_of_white_predominant_regions))
+    pixel_count_cut = int(pixel_count_end_of_first_painting + ((pixel_count_start_of_second_painting - pixel_count_end_of_first_painting) / 2))
+    return number_of_white_predominant_regions, pixel_count_cut
 
 def splitImageInTwo(img_path, cutPoint):
     print("\nSplitting img_path: " + img_path + " at cutPoint " + str(cutPoint))
     img = cv2.imread(img_path)
-    orig_shape = img.shape
-    orig_width = orig_shape[1]
-    orig_height = orig_shape[0]
-    scaled_cut_point = int((cutPoint / RESIZE_SHAPE[1]) * orig_width)
-    img1 = img[:, 0: scaled_cut_point]
-    img2 = img[:, scaled_cut_point: orig_width]
-    images = [img1, img2]
+    img1 = img[:,:cutPoint,:]
+    img2 = img[:,cutPoint:,:]
     cv2.imwrite(img_path.replace(".jpg", "_cut1.png"), img1)
     cv2.imwrite(img_path.replace(".jpg", "_cut2.png"), img2)
-    return images
-
+    return [img1, img2]
 
 class colorRegion(Enum):
     BLACK = 0,
     WHITE = 1
 
-
-def getListOfPaintings(img_folder):
+def getListOfPaintings(img_folder,method):
     output = []
     for img_path in sorted(glob(os.path.join(img_folder, "*.jpg")))[:]:
-        mask = computeMaskToCountPaingings(img_path)
-        count, cut = countNumberPaintingsBasedOnMask(mask, img_path)
+        if method == "MASK":
+            mask = computeMaskToCountPaintings(img_path)
+            count, cut = countNumberPaintingsBasedOnMask(mask, img_path)
+        elif method == "EDGES":
+            mask = computeEdgesToCountPaintings(img_path)
+            count, cut = countNumberPaintingsBasedOnEdges(mask, img_path)
         if count == 2:
             print("-- Cut at pixel: ", str(cut))
             output.append(splitImageInTwo(img_path, cut))
         else:
-            output.append(cv2.imread(img_path))
+            output.append([cv2.imread(img_path)])
 
 
-def main(img_folder):
+def main(img_folder,method):
     error_images = []
     for img_path in sorted(glob(os.path.join(img_folder, "*.jpg")))[:]:
         expected = 1
-        if img_path in ["../qsd2_w2/00001.jpg", "../qsd2_w2/00004.jpg", "../qsd2_w2/00009.jpg", "../qsd2_w2/00017.jpg",
-                        "../qsd2_w2/00018.jpg", "../qsd2_w2/00025.jpg", "../qsd2_w2/00028.jpg"]:
+        if any([item in img_path for item in ["00001.jpg","00004.jpg","00009.jpg","00017.jpg","00018.jpg","00025.jpg","00028.jpg"]]):
             expected = 2
 
-        mask = computeMaskToCountPaingings(img_path)
-        count, cut = countNumberPaintingsBasedOnMask(mask, img_path)
+        if method == "MASK":
+            mask = computeMaskToCountPaintings(img_path)
+            count, cut = countNumberPaintingsBasedOnMask(mask, img_path)
+        elif method == "EDGES":
+            mask = computeEdgesToCountPaintings(img_path)
+            count, cut = countNumberPaintingsBasedOnEdges(mask, img_path)
 
         if count == expected:
             print("-- Good guess for image: ", img_path)
@@ -126,8 +208,10 @@ def main(img_folder):
 
 
 if __name__ == '__main__':
-    main("../qsd2_w2")
+    imgs_folder = r"C:\Users\PC\Documents\Roger\Master\M1\Project\Week2\qsd2_w2"
+    main(imgs_folder,method="EDGES")
 
+# METHOD MASK
 '''Anotate results:
 Note that we prefer to predict 2 in one, that missing a picture
 For params: 0.6, 0.5 => 00000.jpg bad guess counts 2 instead of 1, 00017.jpg bad guess counts 1 instead of 2. Seems hard to correct both
@@ -137,4 +221,10 @@ For params: 0.7, 0.5 => Fix 00000.jpg but breaks 00004.jpg, 000018 and 00025 and
 For params: 0.65, 0.5 => Fix 000018.jpg and 00004.jpg but breaks back 00000.jpg, and still has 00017 and 00018 wrong
 
 With params 0.6, 0.5 => Change the kernel size from (10,10) (40,40) to a (30,30), (40,40) => Bad guess in 4.jpg, 9.jpg, 28.jpg
+'''
+
+# METHOD EDGES
+'''Results annotation:
+It only fails for image 18 because it has a horizontal structure in the bottom of the image that prevents detecting the division
+between the two paintings.
 '''
