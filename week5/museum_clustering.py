009553a9dc7edcf5f0f59a468187f296f198e9cd
Joan Fontanals Martinez <jfontanalsmartinez@gmail.com>
2019-11-05 23:04:10 +0100

    Return closest to centroid for each cluster

--- week5/museum_clustering.py
+++ week5/museum_clustering.py
@@ -6,10 +6,14 @@ import os
 import time
 from sklearn.cluster import KMeans
 import numpy as np
+import shutil
+from sklearn.metrics import pairwise_distances_argmin_min
 
 # -- DIRECTORIES -- #
 db_path = "../bbdd"
 res_root = "../results"
+cluster_root = "../cluster"
+NUM_CLUSTERS = 10
 
 
 def main():
@@ -17,7 +21,8 @@ def main():
 
     print("\nLoading bbdd images...")
     start = time.time()
-    bbdd_images = [[cv2.imread(item)] for item in sorted(glob(os.path.join(db_path, "*.jpg"))[:])]
+    bbdd_images_files = sorted(glob(os.path.join(db_path, "*.jpg"))[:])
+    bbdd_images = [[cv2.imread(item)] for item in bbdd_images_files]
     bbdd_images = [[cv2.resize(item[0], (1000, 1000))] for item in bbdd_images]
     print(len(bbdd_images))
     print("Done. Time: " + str(time.time() - start))
@@ -27,22 +32,45 @@ def main():
     start = time.time()
     bbdd_descriptor = TransformDescriptor(bbdd_images, None, None)
     bbdd_results = bbdd_descriptor.compute_descriptors(transform_type='hog')
-    for k,v in bbdd_results.items():
-        x = np.array(v)
-        print(x.shape)
-    X = np.array([np.array(result) for (k, result) in bbdd_results.items()])
+    X = np.array([np.array(result).reshape(-1) for (k, result) in bbdd_results.items()])
     print(X.shape)
-    kmeans = KMeans(n_clusters=10, random_state=0).fit(X)
+    print("\nComputing Kmeans clustering...")
+    kmeans = KMeans(n_clusters=NUM_CLUSTERS, random_state=0).fit(X)
     print(kmeans.labels_)
 
-    #print(type(bbdd_results))
-    #print(len(bbdd_results))
-    #print(bbdd_results[0].len)
-    #print(len(bbdd_results[1]))
-    #print(type(bbdd_results[0]))
-    #print(type(bbdd_results[1]))
-    #print(bbdd_results[0])
-    #print(bbdd_results[1])
+    for (idx, cluster_value) in enumerate(kmeans.labels_):
+        file = bbdd_images_files[idx]
+        shutil.copy2(file, cluster_root + "/" + str(cluster_value + 1))
+
+    centers = np.array(kmeans.cluster_centers_)
+    clusters = kmeans.labels_.tolist()
+
+    closest_data = []
+    for i in range(NUM_CLUSTERS):
+        print("i " + str(i))
+        center_vec = centers[i].reshape(1, -1)
+        data_idx_within_i_cluster = [idx for idx, clu_num in enumerate(clusters) if clu_num == i]
+        cluster_size = len(data_idx_within_i_cluster)
+        if cluster_size > 0:
+            print("a " + str(len(data_idx_within_i_cluster)))
+            print("b " + str(centers.shape))
+            X_cluster = np.zeros((len(data_idx_within_i_cluster), centers.shape[1]))
+            print("c " + str(X_cluster.shape))
+            for row_num, data_idx in enumerate(data_idx_within_i_cluster):
+                one_row = X[data_idx]
+                X_cluster[row_num] = one_row
+
+            closest, _ = pairwise_distances_argmin_min(center_vec, X_cluster)
+            closest_to_find = min(5, cluster_size)
+            closest_idx_in_one_cluster_tf_matrix = closest[0]
+            closest_data_row_num = data_idx_within_i_cluster[closest_idx_in_one_cluster_tf_matrix]
+            data_id = bbdd_images_files[closest_data_row_num]
+            print("Closest images to cluster center for cluster " + str(i))
+            print(data_id)
+            closest_data.append(data_id)
+
+    print(closest_data)
+
     print("Done. Time: " + str(time.time() - start))
 
 
